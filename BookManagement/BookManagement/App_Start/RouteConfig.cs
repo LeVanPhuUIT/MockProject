160121e57a6deb63ce65c75c164452bb1edeef77
Le Van Phu (FA.G0) <PhuLV2@fsoft.com.vn>
2018-05-09 13:48:47 +0700

    init

---
+++ BookManagement/BookManagement/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BookManagement
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routing to Auther
+            routes.MapRoute(
+                name: "Author",
+                url: "Author",
+                defaults: new { controller = "Author", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
