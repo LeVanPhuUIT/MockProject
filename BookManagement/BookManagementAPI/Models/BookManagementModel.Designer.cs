dd02f8937f5c7061c0165c6946dd55b8f1ff1b63
LeVanPhuUIT <phulevan1995@gmail.com>
2018-05-16 18:54:02 +0700

    a

--- BookManagement/BookManagementAPI/Models/BookManagementModel.Designer.cs
+++ BookManagement/BookManagementAPI/Models/BookManagementModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\phulv2\Desktop\MyWorkSpace\MockProject\BookManagement\BookManagementAPI\Models\BookManagementModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\FPT\MockProject\BookManagement\BookManagementAPI\Models\BookManagementModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
