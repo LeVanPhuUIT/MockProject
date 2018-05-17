827a6827722f059aadd54e75ce781105d3572241
LeVanPhuUIT <phulevan1995@gmail.com>
2018-05-18 01:53:35 +0700

    fix path length image

--- BookManagement/BookManagementAPI/Controllers/BooksController.cs
+++ BookManagement/BookManagementAPI/Controllers/BooksController.cs
@@ -194,7 +194,7 @@ namespace BookManagementAPI.Controllers
             //Upload Image
             var postedFile = httpRequest.Files["Image"];
             //Create custom filename
-            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+            imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).ToArray()).Replace(" ", "-");
             imageName = imageName /*+ DateTime.Now.ToString("yymmssfff")*/ + Path.GetExtension(postedFile.FileName);
             var filePath = HttpContext.Current.Server.MapPath("~/Image/" + imageName);
             postedFile.SaveAs(filePath);
